2d0fe631adbe4ea389600ca831ce2c8fd2ba6fd6
Shpoodr <heyheylogan.m@gmail.com>
2025-08-23 18:03:11 -0700

    created stateMachine blueprints

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     public float moveSpeed = 5f;
+    public float dashSpeed = 100f;
     private CharacterController characterController;
     public Transform playerVisuals;
     Rigidbody rBody;
@@ -18,7 +19,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    public float rotationSpeed = 700f; 
+    public float rotationSpeed = 700f;
     void Update()
     {
         //getting the inputs for later use
@@ -26,14 +27,18 @@ public class PlayerMovement : MonoBehaviour
         float verticalMovement = Input.GetAxisRaw("Vertical");
 
         Vector3 moveDirection = new Vector3(horizontalMovement, 0f, verticalMovement);
-
+        
         characterController.Move(moveDirection * moveSpeed * Time.deltaTime);
-        Debug.Log(moveDirection);
         if (moveDirection.sqrMagnitude > 0.01f)
         {
             Quaternion rotation = Quaternion.LookRotation(moveDirection);
             //transform.rotation = rotation;
             playerVisuals.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("space clicked");
+            characterController.Move(moveDirection * dashSpeed * Time.deltaTime);
         }
     }
 }
---
+++ Assets/Scripts/StateMachine/StateManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public abstract class StateManager<EState> : MonoBehaviour where EState : Enum
+{
+    protected Dictionary<EState, BaseState<EState>> States = new Dictionary<EState, BaseState<EState>>();
+    protected BaseState<EState> CurrentState;
+    protected bool IsTransitioningState = false;
+
+    void Start()
+    {
+        CurrentState.EnterState();
+    }
+    void Update()
+    {
+        EState nextStateKey = CurrentState.GetNextState();
+        if (nextStateKey.Equals(CurrentState.StateKey))
+        {
+            CurrentState.UpdateState();
+        }
+        else
+        {
+            TransitionToState(nextStateKey);
+        }
+        CurrentState.UpdateState();
+    }
+
+    public void TransitionToState(EState StateKey)
+    {
+        IsTransitioningState = true;
+        CurrentState.ExitState();
+        CurrentState = States[StateKey];
+        CurrentState.EnterState();
+        IsTransitioningState = false;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        CurrentState.OnTriggerEnter(other);
+    }
+    void OnTriggerStay(Collider other)
+    {
+        CurrentState.OnTriggerStay(other);
+    }
+    void OnTriggerExit(Collider other)
+    {
+        CurrentState.OnTriggerExit(other);
+    }
+}
